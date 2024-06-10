1382d423b14efc487a2e5dea095ff698f8fd4df0
madymona <madymona@gmail.com>
2024-06-10 15:28:37 -0400

    fixes

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const noteTitleInput = document.getElementById('note-title')
+    const noteContentInput = document.getElementById('note-content')
+    const noteCategoryInput = document.getElementById('note-category')
+    const noteForm = document.getElementById('note-form')
+    const notesList = document.getElementById('notes-list')
+    const searchInput = document.getElementById('search-input')
+})
